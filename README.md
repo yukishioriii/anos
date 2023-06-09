27e33038cc60a90f8d2921e2ca7950f9824ef78a
ando <anmana000@gmail.com>
2023-06-09 16:47:10 +0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # anos
 an-os
+```
+nasm -f bin wac.asm -o wac.bin && qemu -drive format=raw,file=wac.bin
+```
--- wac.asm
+++ wac.asm
@@ -1,13 +1,25 @@
+[ org 0x7c00 ]
+
 mov ah, 0x0e
-mov al, 'H'
-mov al, 'a'
-mov al, 'v'
-mov al, 'e'
-mov al, 's'
-mov al, 'e'
+
+; mov al, the_fuck
+; int 0x10
+
+mov al, [the_fuck]
 int 0x10
-mov al, 'x'
+
+mov bx, the_fuck
+add bx, 0x0
+mov al, [bx]
 int 0x10
+
+mov al, [0x7c09]
+int 0x10
+
+jmp $
+
+the_fuck:
+    db "X"
 
 
 times 510-($-$$) db 0
